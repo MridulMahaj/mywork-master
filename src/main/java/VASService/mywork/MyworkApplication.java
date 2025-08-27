1c036cf78702c221663594118d531e5e45195aed
MridulMahaj <coolwizard1017@gmail.com>
2025-08-27 17:59:50 +0530

    Added main controller class

---
+++ src/main/java/VASService/mywork/MyworkApplication.java
@@ -0,0 +1,20 @@
+package VASService.mywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//TO DO
+//User Authentication
+//Real time subscription status updates
+
+//Integrate backend service for VAS
+//Implement real time updates
+@SpringBootApplication
+public class MyworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyworkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/VASService/mywork/controller/MainController.java
@@ -0,0 +1,50 @@
+package VASService.mywork.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MainController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    // Cricket Service
+    @GetMapping("/cricket")
+    public String getCricketData() {
+        String url = "http://localhost:8083/matches/live";
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    /*
+    // Movie Service
+    @GetMapping("/movies")
+    public String getMovieData() {
+        String url = "http://localhost:8082/api/movies";
+        return restTemplate.getForObject(url, String.class);
+    }*/
+
+    /*
+
+    // Tetris Service
+    @GetMapping("/tetris")
+    public String getTetrisData() {
+        String url = "http://localhost:8083/api/tetris";
+        return restTemplate.getForObject(url, String.class);
+    }
+    */
+    // Weather Service
+    @GetMapping("/weather")
+    public String getWeatherData(@RequestParam String city) {
+        String url = "http://localhost:8082/api/weather/current?city=" + city;
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    // News Service
+    @GetMapping("/news")
+    public String getNewsData(@RequestParam String story) {
+        String url = "http://localhost:8081/api/news/full-story?story=" + story;
+        return restTemplate.getForObject(url, String.class);
+    }
+}
