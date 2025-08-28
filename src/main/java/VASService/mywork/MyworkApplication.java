f040b851ec98a9c34ce63c59bf37ff0791f8bbe4
MridulMahaj <coolwizard1017@gmail.com>
2025-08-28 21:25:14 +0530

    Added WebSocket for real time subscription status and real time ui updates

--- src/main/java/VASService/mywork/MyworkApplication.java
+++ src/main/java/VASService/mywork/MyworkApplication.java
@@ -4,12 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-//TO DO
-//User Authentication
-//Real time subscription status updates
 
-//Integrate backend service for VAS
-//Implement real time updates
 @SpringBootApplication
 public class MyworkApplication {
 
